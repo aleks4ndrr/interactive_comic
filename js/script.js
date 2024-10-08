6c0fc226ce0f008397bc6a37d5b7fa34125abb5c
o3010a7 <o3010a7@gmail.com>
2024-10-08 11:04:15 +0400

    added more html, css, js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+let arrow_down;
+let panel_one;
 //initialize AOS when document loaded
-$(function () {
+window.addEventListener("DOMContentLoaded", () => {
   AOS.init();
+  arrow_down = document.getElementById("arrow");
+  panel_one = document.getElementById("first");
+
+  // check if elements exist before adding event listener
+  if (arrow_down && panel_one) {
+    arrow_down.addEventListener("click", () => {
+      panel_one.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+  }
 });
